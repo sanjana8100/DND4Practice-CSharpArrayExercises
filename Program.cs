6bdf60d0f97809c4fe68dca9018b8fe3d946170c
sanjana8100 <sanjanams812@gmail.com>
2023-06-12 16:07:27 +0530

    added: P2

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@
             //P1- Store elements in an array and print them
             Program1 p1 = new Program1();
             p1.StoreAndPrintArray();
+
+            //P2- Read n values in an array and display them in reverse order
+            Program2 p2 = new Program2();
+            p2.StoreAndPrintArrayInReverse();
         }
     }
 }
---
+++ Program2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNDPractice_CSharpArrayExercises
+{
+    internal class Program2
+    {
+        public void StoreAndPrintArrayInReverse()
+        {
+            Console.WriteLine("Please enter the number of elements in the Array: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[length];
+
+            Console.WriteLine($"Please enter {length} elements of the Array: ");
+            for (int index = 0; index < array.Length; index++)
+            {
+                array[index] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("The Elements in the Array in Reverse Order: ");
+            for (int index = length-1; index >= 0; index--)
+            {
+                Console.Write(array[index] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
