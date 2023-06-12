b496e4a5d351620036ae8c234b8334cde1ae603c
sanjana8100 <sanjanams812@gmail.com>
2023-06-12 16:17:23 +0530

    added: P3

--- Program.cs
+++ Program.cs
@@ -4,13 +4,20 @@
     {
         static void Main(string[] args)
         {
-            //P1- Store elements in an array and print them
-            Program1 p1 = new Program1();
-            p1.StoreAndPrintArray();
+            //Console.WriteLine("P1- Store elements in an array and print them");
+            //Console.WriteLine("----------------------------------------------------------");
+            //Program1 p1 = new Program1();
+            //p1.StoreAndPrintArray();
 
-            //P2- Read n values in an array and display them in reverse order
-            Program2 p2 = new Program2();
-            p2.StoreAndPrintArrayInReverse();
+            //Console.WriteLine("P2- Read n values in an array and display them in reverse order");
+            //Console.WriteLine("----------------------------------------------------------");
+            //Program2 p2 = new Program2();
+            //p2.StoreAndPrintArrayInReverse();
+
+            Console.WriteLine("P3- Find the sum of all array elements");
+            Console.WriteLine("----------------------------------------------------------");
+            Program3 p3 = new Program3();
+            p3.SumOfElementsInArray();
         }
     }
 }
---
+++ Program3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNDPractice_CSharpArrayExercises
+{
+    internal class Program3
+    {
+        public void SumOfElementsInArray()
+        {
+            Console.WriteLine("Please enter the number of elements in the Array: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[length];
+
+            Console.WriteLine($"Please enter {length} elements of the Array: ");
+            for (int index = 0; index < array.Length; index++)
+            {
+                array[index] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int sum = 0;
+            for (int index = 0; index < length; index++)
+            {
+                sum += array[index];
+            }
+            Console.WriteLine($"The Sum of all the elements in the Array is: {sum}");
+            Console.WriteLine();
+        }
+    }
+}
